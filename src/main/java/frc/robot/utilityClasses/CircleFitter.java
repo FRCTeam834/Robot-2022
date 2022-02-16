020c9759382b409a72207160d59df41a78bc6893
realTronsi <ks.yan@yahoo.com>
2022-02-15 20:16:46 -0500

    Added working circlefitter

--- src/main/java/frc/robot/utilityClasses/CircleFitter.java
+++ src/main/java/frc/robot/utilityClasses/CircleFitter.java
@@ -1,74 +1,85 @@
 package frc.robot.utilityClasses;
 
-import org.ejml.simple.SimpleMatrix;
-
 import java.util.List;
 
+import org.ejml.simple.SimpleMatrix;
+
 public class CircleFitter {
-    static List<VisionPoint> circlePoints;
-    static double radius = 0;
-    static double[] computerSums = new double[8];
-    static double totalX;
-    static double totalY;
-    static double totalXSquared;
-    static double totalYSquared;
-    static double totalXY;
+    static List<GlobalPoint> circlePoints;
+    static double xsum;
+    static double ysum;
+    static double xSquaredsum;
+    static double ySquaredsum;
+    static double xysum;
 
     static double index1;
     static double index2;
     static double index3;
 
     // declare the matrix
-    static SimpleMatrix a = new SimpleMatrix(3, 3);
-    static SimpleMatrix b = new SimpleMatrix(3, 1);
-    static SimpleMatrix c = new SimpleMatrix(2, 1);
+    static SimpleMatrix X = new SimpleMatrix(3, 3);
+    static SimpleMatrix Y = new SimpleMatrix(3, 1);
 
-    public void CircleFitter(List<VisionPoint> points, double radius) {
+    static double A;
+    static double B;
+    static double C;
+
+    public CircleFitter() { }
+
+    public void setPoints(List<GlobalPoint> points) {
         circlePoints = points;
-        this.radius = radius;
-    }
+        // calculate matrices
+        xsum = ysum = xSquaredsum = ySquaredsum = xysum = index1 = index2 = index3 = 0;
 
-    private static void computeSums() {
-        for (int i = 0; i < circlePoints.size(); i++) {
-            totalX += circlePoints.get(i).getX();
-            totalXSquared += Math.pow(circlePoints.get(i).getY(), 2);
-            totalY += circlePoints.get(i).getY();
-            totalYSquared += Math.pow(circlePoints.get(i).getY(), 2);
-            totalXY += circlePoints.get(i).getX() * circlePoints.get(i).getX();
-            index1 +=
-                    circlePoints.get(i).getX()
-                            * (Math.pow(circlePoints.get(i).getX(), 2)
-                                    + Math.pow(circlePoints.get(i).getY(), 2));
-            index2 +=
-                    circlePoints.get(i).getY()
-                            * (Math.pow(circlePoints.get(i).getX(), 2)
-                                    + Math.pow(circlePoints.get(i).getY(), 2));
-            index3 +=
-                    (Math.pow(circlePoints.get(i).getX(), 2)
-                            + Math.pow(circlePoints.get(i).getY(), 2));
+        for(int i = 0; i < points.size(); i++) {
+            GlobalPoint point = points.get(i);
+            double x = point.x;
+            double y = point.y;
+
+            xsum += x;
+            ysum += y;
+            xSquaredsum += x * x;
+            ySquaredsum += y * y;
+            xysum += x * y;
+
+            index1 += x * (x * x + y * y);
+            index2 += y * (x * x + y * y);
+            index3 += x * x + y * y;
         }
+        // set matrices
+        X.set(0, 0, xSquaredsum);
+        X.set(0, 1, xysum);
+        X.set(0, 2, xsum);
+
+        X.set(1, 0, xysum);
+        X.set(1, 1, ySquaredsum);
+        X.set(1, 2, ysum);
+
+        X.set(2, 0, xsum);
+        X.set(2, 1, ysum);
+        X.set(2, 2, circlePoints.size());
+
+        Y.set(0, 0, index1);
+        Y.set(1, 0, index2);
+        Y.set(2, 0, index3);
     }
 
-    private static double[] computeCenter() {
-        a.set(0, 0, totalXSquared);
-        a.set(0, 1, totalXY);
-        a.set(0, 2, totalX);
-        a.set(1, 0, totalXY);
-        a.set(1, 1, totalYSquared);
-        a.set(1, 2, totalY);
-        a.set(2, 0, totalX);
-        a.set(2, 1, totalY);
-        a.set(2, 2, circlePoints.size());
+    public double[] calculateCircle() {
+        double[] ret = new double[3];
 
-        b.set(0, 0, index1);
-        b.set(1, 0, index2);
-        b.set(2, 0, index3);
-        c = a.invert().mult(b);
-        return new double[2];
-    }
+        SimpleMatrix M = X.invert().mult(Y);
 
-    public static void main(String args[]) {
-        computeSums();
-        computeCenter();
+        A = M.get(0);
+        B = M.get(1);
+        C = M.get(2);
+
+        // k value
+        ret[0] = A / 2;
+        // m value
+        ret[1] = B / 2;
+        // radius
+        ret[2] = Math.sqrt(4 * C + A * A + B * B) / 2;
+
+        return ret;
     }
 }
