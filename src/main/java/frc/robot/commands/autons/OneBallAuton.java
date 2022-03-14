c250ca9ed456c4aa8b68925067508b84c4355980
cap1sup <xmanpiper@gmail.com>
2022-03-14 08:59:57 -0400

    Fix minor error due to naming

--- src/main/java/frc/robot/commands/autons/OneBallAuton.java
+++ src/main/java/frc/robot/commands/autons/OneBallAuton.java
@@ -37,9 +37,9 @@ import frc.robot.subsystems.climber.HomeClimberTubes;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class TwoBallAuton extends SequentialCommandGroup {
+public class OneBallAuton extends SequentialCommandGroup {
 
-    public TwoBallAuton() {
+    public OneBallAuton() {
         // Add your commands in the addCommands() call, e.g.
         // addCommands(new FooCommand(), new BarCommand());
         addCommands(
