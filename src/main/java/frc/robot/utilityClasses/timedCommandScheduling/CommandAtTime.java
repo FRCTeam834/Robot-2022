9617c5af08ceb0d3f4904f6805a1bf5bd356beda
Christian Piper <xmanpiper@gmail.com>
2022-03-26 23:42:02 -0400

    Wrote framework for timed command scheduling
    Needs extensive testing

---
+++ src/main/java/frc/robot/utilityClasses/timedCommandScheduling/CommandAtTime.java
@@ -0,0 +1,36 @@
+package frc.robot.utilityClasses.timedCommandScheduling;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class CommandAtTime {
+
+    // Variables to store the command and time
+    private Command command;
+    private double time;
+
+    /**
+     * A command that is scheduled to run at a specific time
+     * @param command The command to run
+     * @param runAt The time (ms, according to FPGA) to run the command at
+     */
+    public CommandAtTime(Command command, double runAt) {
+        this.command = command;
+        time = runAt;
+    }
+
+    /**
+     * Gets the time that the command should run at
+     * @return time to run at (ms)
+     */
+    public double getTime() {
+        return time;
+    }
+
+    /**
+     * Gets the command to run
+     * @return the command to run
+     */
+    public Command getCommand() {
+        return command;
+    }
+}
---
+++ src/main/java/frc/robot/utilityClasses/timedCommandScheduling/TimedCommandSchedule.java
@@ -0,0 +1,82 @@
+package frc.robot.utilityClasses.timedCommandScheduling;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class TimedCommandSchedule {
+    private ArrayList<CommandAtTime> commands;
+
+    public TimedCommandSchedule() {
+        commands = new ArrayList<CommandAtTime>();
+    }
+
+    public void add(CommandAtTime commandAtTime) {
+
+        // Get the size of the commands list
+        int size = commands.size();
+
+        // If the arraylist is empty, just insert it
+        if (size == 0) {
+            commands.add(commandAtTime);
+        }
+        // ! WRITE YET
+        else {
+            // We gotta do a whole binary search for the right location
+            commands.add(binarySearch(commandAtTime.getTime()), commandAtTime);
+        }
+    }
+
+    private int binarySearch(double newTime) {
+        int low = 0;
+        int high = commands.size() - 1;
+
+        while (low <= high) {
+
+            // Finding the mid using floor division
+            int mid = low + ((high - low) / 2);
+
+            // Get the time of the mid command
+            double midTime = commands.get(mid).getTime();
+
+            // If the time of the middle command is the same, schedule it directly after the middle command
+            if (midTime == newTime) {
+                return mid + 1;
+            }
+
+            // Check if the mid's time is less than the new command
+            else if (midTime < newTime) {
+                low = mid;
+            }
+
+            // Check if the mid's time is greater than the new command
+            else if (midTime > newTime) {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    public void add(Command command, double atTime) {
+        add(new CommandAtTime(command, atTime));
+    }
+
+    public void add(Runnable runnable, double atTime) {
+        add(new InstantCommand(runnable), atTime);
+    }
+
+    public void add(Runnable runnable, double atTime, Subsystem... requirements) {
+        add(new InstantCommand(runnable, requirements), atTime);
+    }
+
+    public void update(double currentTime) {
+        while (commands.get(0).getTime() <= currentTime) {
+            CommandScheduler.getInstance().schedule(commands.remove(0).getCommand());
+        }
+    }
+}
